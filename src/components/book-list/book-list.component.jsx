a9bf52c7e4e3b373a7db34d6f340c99e2571e0e5
Christian <christian_corfixen@outlook.com>
2021-02-03 11:09:14 +0100

    Moved fetching of book data into a custom hook for better reuseability. Changed book-list component to reflect changes

--- src/components/book-list/book-list.component.jsx
+++ src/components/book-list/book-list.component.jsx
@@ -1,24 +1,20 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
+import useBookData from '../../hooks/useBookData';
 
-import { FetchData } from '../../api/data.service';
 import BookCard from '../book-card/book-card.component';
 
 const BookList = () => {
-  const [books, setBooks] = useState(false);
+  const { data: books, error } = useBookData('codetest-bookdata');
 
   useEffect(() => {
-    //Fake api call
-    FetchData('codetest-bookdata')
-    .then((response) => {
-      console.log(response);
-      setBooks(response.data.books);
-    });
-  }, []);
+    console.log(books);
+  }, [books]);
 
   return(
     <div className="book-list">
       { books && books.map((book, i) => <BookCard key={i} book={book} />)}
       { !books && <p>You have no books!</p> }
+      { error && <p>{ error }</p> }
     </div>
   )
 }
---
+++ src/hooks/useBookData.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+
+import { FetchData } from '../api/data.service';
+
+const useBookData = ({url}) => {
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    //Fake api call
+    FetchData(url)
+    .then((response) => {
+      console.log(response);
+      setData(response.data.books);
+    })
+    .catch((error) => {
+      setError(error);
+    });
+  }, [url]);
+
+  return {
+    data,
+    error
+  }
+}
+
+export default useBookData;
